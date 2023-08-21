5f426adf2fcdf0d7836c8dc93d3d7b39e2aeb1fb
Gabriel Ilton <gabrielssantosjau@gmail.com>
2023-08-20 22:26:15 -0300

    feat: add environments

---
+++ src/config/Environments.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // C
+  CLASS_LIMIT: process.env.CLASS_LIMIT,
+  // D
+  DATABASE_URI: process.env.DATABASE_URI,
+  // N
+  NODE_ENV: process.env.NODE_ENV ?? 'development',
+  // P
+  PORT: process.env.PORT ?? '3030'
+};
